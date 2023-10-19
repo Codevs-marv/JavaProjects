fbbff230052823eb505653ac4aa875fd2360dde4
MarcoRodriguez <marcoansrv19@gmail.com>
2023-10-18 22:29:11 -0500

    PromoAlmacen

--- Estructura_DecLogica/Exercice_13.java
+++ Estructura_DecLogica/Exercice_13.java
@@ -1,3 +1,15 @@
+/*
+Un almacén efectúa una promoción en la cual se hace un descuento sobre el valor de la
+compra total, según el color de la bolita que el cliente saque al pagar en caja. Si la bolita
+es blanca no se le hará descuento alguno, si es verde se le hará un 10% de descuento, si es
+amarilla un 25%, si es azul un
+50% y si es roja un 100%. Hacer un algoritmo para determinar la cantidad final que un
+cliente deberá pagar por su compra. Se sabe que sólo hay bolitas de los colores
+mencionados.
+ */
+
+
+
 package Estructura_DecLogica;
 import java.util.Scanner;
 
